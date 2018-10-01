867f90a481795af3c133ef9ed39fb9b06f9f253c
a6672634 <a6672634@yandex.ru>
2018-10-01 23:41:38 +0300

    ввв

--- src/organization/itstep/qa/Test.java
+++ src/organization/itstep/qa/Test.java
@@ -7,8 +7,7 @@ public class Test {
         Rectangle rectangle = new Rectangle(1,2,3,3);
         Rectangle rectangle1 = new Rectangle(4,4);
         Rectangle rectangle2 = new Rectangle();
-        System.out.println(rectangle.getX1());
-                System.out.println(rectangle1);
-        System.out.println(rectangle2);
+        System.out.println(rectangle);
+
     }
 }
